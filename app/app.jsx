124a419133daec17ff689b77e29e5e6cbf6b1618
jwilliamsmith <perfiction@gmail.com>
2017-01-26 13:48:06 -0500

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,15 @@ const ReactDOM = require('react-dom');
 const { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
 const TodoApp = require('TodoApp');
+const actions = require('actions');
+const store = require('configureStore').configure();
+
+store.subscribe(() => {
+	console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean up the yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 //App CSS
 require('style!css!sass!applicationStyles');
---
+++ app/store/configureStore.jsx
@@ -0,0 +1,14 @@
+'use strict';
+const redux = require('redux');
+const {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
+export const configure = () => {
+  let reducer = redux.combineReducers({
+    searchText: searchTextReducer,
+    showCompleted: showCompletedReducer,
+    todos: todosReducer
+  });
+  let store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+  return store;
+};
