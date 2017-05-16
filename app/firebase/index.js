8eeeccd22c3ff216734e5e6602d9c754f9737e95
jwilliamsmith <perfiction@gmail.com>
2017-05-16 12:23:08 -0400

    move fb creds to env vars

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -4,11 +4,11 @@ import firebase from 'firebase';
 
 try {
   const config = {
-    apiKey: "AIzaSyAJWdJndJuT9irnrXzvwu6xANhWFPMiWxg",
-    authDomain: "jws-todo-app.firebaseapp.com",
-    databaseURL: "https://jws-todo-app.firebaseio.com",
-    storageBucket: "jws-todo-app.appspot.com",
-    messagingSenderId: "322058385785"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
@@ -17,3 +17,5 @@ try {
 
 export const fbref = firebase.database().ref();
 export default firebase;
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 'use strict';
 const webpack = require('webpack');
 const path = require('path');
+const envFile = require('node-env-file');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+try {
+  envFile(path.join(__dirname, 'config/'+process.env.NODE_ENV+'.env'));
+} catch (e) {
+
+}
 
 module.exports = {
   entry: [
@@ -22,6 +29,16 @@ module.exports = {
       compressor: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        STORAGE_BUCKET: JSON.stringify(process.env.DATABASE_URL),
+        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)      
+      }    
     })
   ],
   output: {
